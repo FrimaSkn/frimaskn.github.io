5a0368a72a2797480cc8e4aba5105d8f5cec66b9
FrimaSkn <frimasatria347@gmail.com>
2022-02-16 15:02:45 +0700

    95%

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,31 +210,11 @@ document.onkeydown = function(e) {
         return false;
         }
     }
+    
+    const options = {
+        speed: 500,
+        speedAsDuration: true,
+        offset: window.innerWidth < 768 ? 400 : 90
+    }
+    new SmoothScroll('a[href*="#"]', options);
 
-
-
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
